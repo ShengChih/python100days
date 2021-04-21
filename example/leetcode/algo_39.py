d8e7367da5b7441c4bf1210c5772878b7caa5370
ShengChih <ms0529756@gmail.com>
2021-04-21 07:24:57 +0000

    add: leetcode algorithm 39. Combination Sum

---
+++ example/leetcode/algo_39.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+from typing import List
+
+class Solution:
+    def findCombination(self, target: int, picks: List[int]):
+        if target < 0:
+            return
+
+        for item in self.candidates:
+            if target < item:
+                return
+
+            if picks and picks[-1] > item:
+                continue
+
+            if target == item:
+                picks += [item]
+                self.output.append(picks)
+                return
+
+            if target > item:
+                self.findCombination(target - item, picks + [item])
+
+        return
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        sortedCandidates =  sorted(candidates)
+        self.candidates = sortedCandidates
+        self.len = len(self.candidates)
+        self.output = []
+        self.check = {}
+
+        result = []
+        picks = []
+
+        self.findCombination(target, picks)
+
+        return self.output
+
+def main():
+    solution = Solution()
+    #print(solution.combinationSum([1], 1))
+    #print(solution.combinationSum([2,3,6,7], 7))
+    #print(solution.combinationSum([2,3,5], 8))
+
+
+if __name__ == '__main__':
+    main()
