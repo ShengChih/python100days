936b0c191c1a7e8626fe87a46a71675241b4f7cc
ShengChih <ms0529756@gmail.com>
2021-05-03 21:09:49 +0800

    improve: bubble sorting boundary problem

--- example/classical_algorithm/sorting/bubble_sort.py
+++ example/classical_algorithm/sorting/bubble_sort.py
@@ -4,21 +4,21 @@ def bubble_sort(data: list) -> list:
     maxIdx = len(data) - 1
 
     for counter in range(maxIdx):
-        print("counter %d" % counter)
+        print("Loop %s:" % counter)
         idx = 0
         while idx < maxIdx:
             if data[idx] > data[idx + 1]:
                 tmp = data[idx]
                 data[idx] = data[idx + 1]
                 data[idx + 1] = tmp
-
-            idx += 1
             print(data)
+            idx += 1
+        maxIdx -= 1
 
 
     return data
 
 if __name__ == '__main__':
-    data = randomlist = random.sample(range(0, 20), 5)
+    data = randomlist = random.sample(range(0, 100), 10)
     print(data)
     print(bubble_sort(data))
