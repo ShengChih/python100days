63587cb01dccbac4d6b8f4a77050e17588a2a79e
ShengChih <ms0529756@gmail.com>
2021-05-05 22:39:58 +0800

    add: fibonacci with cache

--- example/classical_algorithm/recursive_and_dp/fibonacci_recursive.py
+++ example/classical_algorithm/recursive_and_dp/fibonacci_recursive.py
@@ -7,5 +7,15 @@ def fibonacci(n: int) -> int:
 
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+def fibonacci_with_cache(n: int, memo: dict) -> int:
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fibonacci_with_cache(n - 1, memo) + \
+        fibonacci_with_cache(n - 2, memo)
+
+    return memo[n]
+
 if __name__ == '__main__':
-    print(fibonacci(10))
+    #print(fibonacci(10))
+    print(fibonacci_with_cache(10, {0: 0, 1: 1}))
