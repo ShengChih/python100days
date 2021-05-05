a60665bddf1b6af09d9ade469f9e61de1d4dd833
ShengChih <ms0529756@gmail.com>
2021-05-05 22:53:50 +0800

    add: climb staris with dp

--- example/classical_algorithm/recursive_and_dp/steps_combination.py
+++ example/classical_algorithm/recursive_and_dp/steps_combination.py
@@ -26,6 +26,15 @@ def f_with_cache(n: int, memo: dict) -> int:
     memo[n] = f(n - 1) + f(n - 2) + f(n - 3)
     return memo[n]
 
+def f_dp(n: int) -> int:
+    dp = [0, 1, 2, 4]
+
+    for i in range(4, n + 1):
+        dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
+
+    return dp[n]
+
 if __name__ == '__main__':
     #print(f(10))
-    print(f_with_cache(10, {-1:0, 0:0, 1:1, 2:2, 3:4}))
+    #print(f_with_cache(10, {-1:0, 0:0, 1:1, 2:2, 3:4}))
+    print(f_dp(10))
